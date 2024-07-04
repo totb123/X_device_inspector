ed2e65b13d31a6499858288de7a0cb2a00dfa789
totb123 <100537245+totb123@users.noreply.github.com>
2024-07-04 14:36:44 +0300

    made edits based on review

--- X_device_ins/src/database/crud.py
+++ X_device_ins/src/database/crud.py
@@ -1,7 +1,7 @@
 from src.database.db_init import SQLALCHEMY_DATABASE_URL
 from sqlalchemy.orm import sessionmaker
 from src.database.models import SectorsDMPosition, Multiboard, Board, Inspection, Sector
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 
 
 def get_dm_position(sector_id):
@@ -30,53 +30,43 @@ def get_dm_position(sector_id):
 def insert_inspections(inspection):
     db = get_connection()
     db.begin()
-    last_multiboard = db.query(Multiboard).order_by(Multiboard.multiboard_id.desc()).first()
-    multiboard_set = {res[0] for res in db.query(Board.multiboard_id).filter( # !review! set_ids
+    multiboard_set = {res[0] for res in db.query(Board.multiboard_id).filter(
         Board.datamatrix.in_(list(map(str, inspection.dm_values)))).all()}
     create_new_boards = True
-    for multiboard_set_id in multiboard_set: # !review! убрать board_list
-        board_list = [elem[0] for elem in db.query(Board.datamatrix).filter(Board.multiboard_id ==
-                                                                            multiboard_set_id).all()]
-        board_list_1 = [elem for elem in
-                        db.query(Board.id, Board.datamatrix, Board.multiboard_id).filter(Board.multiboard_id ==
-                                                                                         multiboard_set_id).all()]
-        for board_list_dm in board_list_1:
+    if multiboard_set:
+        for multiboard_set_id in multiboard_set:
+            board_list = [elem for elem in
+                    db.query(Board.id, Board.datamatrix, Board.multiboard_id).filter(Board.multiboard_id ==
+                                                                                             multiboard_set_id).all()]
+            for board_list_dm in board_list:
+                if not create_new_boards:
+                    break
+                new_multiboard_id = board_list_dm[2]
+                for inspection_dm in inspection.dm_values:
+                    if board_list_dm[1] != '0' and board_list_dm[1] == inspection_dm:
+                        create_new_boards = False
+                        break
+                    else:
+                        create_new_boards = True
             if not create_new_boards:
                 break
-            new_multiboard_id = board_list_dm[2]  # !review! убрать магические числа, сделать раскрытие tuple
-            for inspection_dm in inspection.dm_values:
-                if board_list_dm[1] != '0' and board_list_dm[1] == inspection_dm:
-                    create_new_boards = False
-
-                    print(board_list_dm[1], create_new_boards, inspection_dm)
-                    break
-                else:
-                    create_new_boards = True
-        if not create_new_boards:
-            break
-    print(board_list, inspection.dm_values, create_new_boards, new_multiboard_id)
-    for index in range(len(board_list)):
-        if board_list[index] != '0' and inspection.dm_values[index] == '0':
-            inspection.dm_values[index] = board_list[index]
-    print(inspection.dm_values)
+        for index in range(len(board_list)):
+            if board_list[index][1] != '0' and inspection.dm_values[index] == '0':
+                inspection.dm_values[index] = board_list[index][1]
     if create_new_boards:
-        if last_multiboard:
-            new_multiboard_id = last_multiboard.multiboard_id + 1  # !review! id во всех полях сделать типом primary serial key
-        else:
-            new_multiboard_id = 1
-        new_multiboard = Multiboard(multiboard_id=new_multiboard_id)
+        new_multiboard = Multiboard()
         db.add(new_multiboard)
-        db.flush()
-        last_board_id = db.query(Board).order_by(Board.id.desc()).first() # !review! id во всех полях сделать типом primary serial key
-        if last_board_id:
-            new_board_id = last_board_id.id + 1
-        else:
-            new_board_id = 1
+        db.commit()
         for i in range(1, 9):
-            new_board = Board(id=new_board_id + i, multiboard_id=new_multiboard_id,
+            new_board = Board(multiboard_id=new_multiboard.id,
                               datamatrix=inspection.dm_values[i - 1])
             db.add(new_board)
+            db.commit()
         db.flush()
+        new_inspection = Inspection(time=inspection.datetime,
+                                    multiboard_id=new_multiboard.id, url_image=inspection.img_path,
+                                    sector_id=inspection.sector_id, status='UNCHECKED')
+        db.add(new_inspection)
     else:
         update_id = [id_[0] for id_ in db.query(Board.id).
             filter(Board.multiboard_id == new_multiboard_id).order_by(Board.id).all()]
@@ -84,16 +74,10 @@ def insert_inspections(inspection):
             db.query(Board).filter(Board.id == update_id[index]).update(
                 {Board.datamatrix: inspection.dm_values[index]})
         db.commit()
-    last_inspection_id = db.query(Inspection).order_by(Inspection.id.desc()).first()  # !review! id во всех полях сделать типом primary serial key
-
-    if last_inspection_id:
-        new_inspection_id = last_inspection_id.id + 1
-    else:
-        new_inspection_id = 1
-    new_inspection = Inspection(id=new_inspection_id, time=inspection.datetime,
-                                multiboard_id=new_multiboard_id, url_image=inspection.img_path,
-                                sector_id=inspection.sector_id, status='UNCHECKED')
-    db.add(new_inspection)
+        new_inspection = Inspection(time=inspection.datetime,
+                                    multiboard_id=new_multiboard_id, url_image=inspection.img_path,
+                                    sector_id=inspection.sector_id, status='UNCHECKED')
+        db.add(new_inspection)
     try:
         db.commit()
         return True
--- X_device_ins/src/inspector1/idmdecoder.py
+++ X_device_ins/src/inspector1/idmdecoder.py
@@ -2,19 +2,17 @@ from pylibdmtx.pylibdmtx import decode
 import cv2
 import random
 
-# функция для обрезания нужной области фотографии и распознования datamatrix'а
-# !review! Функция должна отвечать за одно действие
-def decode_datamatrix(img, dm_position):
-    list_coordinates = list(map(int, dm_position.split(',')))
 
-    print(list_coordinates)
-    cropped_image = img[list_coordinates[0]:list_coordinates[1], list_coordinates[2]:list_coordinates[3]]  # y1 y2 x1 x2
-    # cv2.imshow("fewf", cropped_image)
-    cv2.imwrite("src/inspector1/image_repo//" + str(random.randint(1, 10000)) + ".jpg", cropped_image)
-    # cv2.waitKey(0)
-    decoded_objects = decode(cropped_image)
+def cropped_image(img, dm_position):
+    list_coordinates = list(map(int, dm_position.split(',')))
+    image = img[list_coordinates[0]:list_coordinates[1], list_coordinates[2]:list_coordinates[3]] # y1 y2 x1 x2
+    return image
+
+
+def decode_datamatrix(img, dm_position):
+    image_decode = cropped_image(img, dm_position)
+    decoded_objects = decode(image_decode)
     if decoded_objects:
-        print(decoded_objects[0].data.decode('utf-8'))
         return str(decoded_objects[0].data.decode('utf-8'))
     else:
         return "0"
@@ -23,10 +21,10 @@ def decode_datamatrix(img, dm_position):
 def choose_side(image, coords, result):
     for index in range(len(coords.top)):
         result_top = decode_datamatrix(image, coords.top[index])
-        result_bot = decode_datamatrix(image, coords.bot[index])
         if result_top != "0":
             result[index] = result_top
             return index + 1, coords.top, "top"
+        result_bot = decode_datamatrix(image, coords.bot[index])
         if result_bot != "0":
             result[index] = result_bot
             return index + 1, coords.bot, "bot"
@@ -41,11 +39,11 @@ def dm_detect(index, true_coords, result, image):
 
 class IDmDetector:
     def __init__(self):
-        self.result = ["0", "0", "0", "0", "0", "0", "0", "0"] # !review! не result, а что-то более понятное
+        self.dm_values_results = ["0", "0", "0", "0", "0", "0", "0", "0"]
 
     def dm_decode(self, image, coords):
-        index, true_coords, side = choose_side(image, coords, self.result)
+        index, true_coords, side = choose_side(image, coords, self.dm_values_results)
         if index is None:
-            return None, self.result
-        dm_detect(index, true_coords, self.result, image)
-        return side, self.result
+            return None, self.dm_values_results
+        dm_detect(index, true_coords, self.dm_values_results, image)
+        return side, self.dm_values_results
