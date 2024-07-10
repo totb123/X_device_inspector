475811002d5584af822991441d06b7276b00d952
Alexey Ivanov <alexeyivanov@MacBook-Alex.local>
2024-07-10 14:03:17 +0300

    add some edits

--- device_inspector_frontend/device_inspector_frontend/src/components/datamatrixGrid.tsx
+++ device_inspector_frontend/device_inspector_frontend/src/components/datamatrixGrid.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row, Spin } from 'antd'
+import { Button, Card, Col, Input, Row, Spin } from 'antd'
 import React from 'react'
 import useBoardsGet from '../features/inspectionsHistory/hooks/useBoardGet'
 
@@ -18,45 +18,52 @@ export const DataMatrix: React.FC<DataMatrixProps> = (
   if (boardsStatus !== 'success') return <Spin/>
   const datamatrixes = boards!.map(board => board.datamatrix)
   return(
-    <Card 
-      type={inner ? 'inner' : undefined}
-      title='Datamatrix-коды плат'>
-      Datamatrix
-      <Row gutter={[8, 8]}>
-        {
-          datamatrixes.map((element, index) => 
-            index < datamatrixes.length / 2
-              ? <Col className='gutter-row' style={
-                highlightedDatamatrix?.filter(
-                  val => val == element.toString()
-                ).length
-                  ? {
-                    color: 'red'
-                  }
-                  : {}
-              }>{index + 1}. {element}</Col>
-              : <></>
-          )
-        }
-      </Row>
-      <Row gutter={[8,8]}>
-        {
-          datamatrixes.map((element, index) => 
-            index >= datamatrixes.length / 2
-              ? <Col className='gutter-row'style={
-                highlightedDatamatrix?.filter(
-                  val => val == element.toString()
-                ).length
-                  ? {
-                    color: 'red'
-                  }
-                  : {}
-              }>{index + 1}. {element}</Col>
-              : <></>
-          )
-        }
-      </Row>
-    </Card>
+    <>
+      <Card 
+        type={inner ? 'inner' : undefined}
+        title='Datamatrix-коды плат'>
+        Datamatrix
+        <Row gutter={[8, 8]}>
+          {
+            datamatrixes.map((element, index) => 
+              index < datamatrixes.length / 2
+                ? <Col className='gutter-row' style={
+                  highlightedDatamatrix?.filter(
+                    val => val == element.toString()
+                  ).length
+                    ? {
+                      color: 'red'
+                    }
+                    : {}
+                }>{index + 1}. <Input defaultValue={element}></Input></Col>
+                : <></>
+            )
+          }
+        </Row>
+        <Row gutter={[8,8]}>
+          {
+            datamatrixes.map((element, index) => 
+              index >= datamatrixes.length / 2
+                ? <Col className='gutter-row'style={
+                  highlightedDatamatrix?.filter(
+                    val => val == element.toString()
+                  ).length
+                    ? {
+                      color: 'red'
+                    }
+                    : {}
+                }>{index + 1}. <Input defaultValue={element}></Input></Col>
+                : <></>
+            )
+          }
+        </Row>
+      </Card>
+      {inner 
+        ? <Button>Edit</Button>
+        : <></>
+      }
+      
+    </>
   )
 }
 
